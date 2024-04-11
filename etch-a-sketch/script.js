f406bbf716394f491d1ecedcabf1a9d65379dc91
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-04-12 04:09:57 +0800

    Figuring out button pressing

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,15 +1,27 @@
 function main()
 {
     const SQUARE_SIDE = 16;
-    const containerDiv = createGrid(SQUARE_SIDE);
+    const containerDiv = createPage(SQUARE_SIDE);
     const gridSquares = containerDiv.querySelectorAll(".grid-square");
 
+    
+    
+    //Color changing
     gridSquares.forEach(gridSquare => {
-        gridSquare.addEventListener("mouseover", () => div.classList.add("paint"));
+        gridSquare.addEventListener("mouseover", () => gridSquare.classList.add("colour"));
     });
+
+    
+    
+    //Button pressing
+    const button = document.querySelector("button");
+    button.textContent = "Choose board size: ";
+    button.addEventListener("click", removeAllChildNodes(containerDiv))
+    console.log(containerDiv);
+
 }
 
-function createGrid(size)
+function createPage(size)
 {    
     const containerDiv = document.createElement("div");
     containerDiv.classList.add("container");
@@ -27,4 +39,55 @@ function createGrid(size)
     return containerDiv;
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 main();
+
+
+
+
+
+
+
+/*
+function main()
+{
+    const SQUARE_SIDE = 16;
+    let size = SQUARE_SIDE;
+
+    createPage(size);
+
+    
+
+    function createPage(size) {
+
+        const button = document.querySelector("button");
+        button.addEventListener("click", changeGrid());
+        
+        const containerDiv = document.createElement("div");
+        containerDiv.classList.add("container");
+
+        for (let i = 0; i < size; i++)
+        {
+            for (let j = 0; j < size; j++)
+            {
+                const gridDiv = document.createElement("div");
+                gridDiv.classList.add("grid-square");
+                containerDiv.appendChild(gridDiv);
+            }
+        }
+        document.body.appendChild(containerDiv);
+    }
+
+    function changeGrid() {
+        let newSize = prompt("Change number of pixels: ");
+        createPage(newSize);
+    }
+}
+
+main()
+*/
