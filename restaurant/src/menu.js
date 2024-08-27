b7725a9c3c29a4eb9a079f1cf7ce83e56b9b4dbc
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-08-28 02:23:14 +0800

    Restructuredgit add ./!

--- restaurant/src/menu.js
+++ restaurant/src/menu.js
@@ -5,10 +5,12 @@ import maggiGorengMenu from './images/menu/maggi-goreng-menu.jpg';
 import cheeseNaanMenu from './images/menu/cheese-naan.jpg';
 import rotiBakarMenu from './images/menu/roti-bakar-menu.jpg';
 import halfBoiledEggsMenu from './images/menu/half-boiled-egg.jpg';
+import tehAisMenu from './images/menu/teh-ais.jpg';
 
 const menu = (function() {
     const initialize = () => {
-
+        const items = getMenuItems();
+        console.log(items);
     }
     
     const getMenuItems = () => {
@@ -31,6 +33,10 @@ const menu = (function() {
         const halfBoiledEggs = createMenuItem('Half-boiled eggs', 2.5, 'Eggs boiled to a soft, creamy yolk', 'Appetizer', halfBoiledEggsMenu);
         menuItems.push(halfBoiledEggs);
 
+        //Beverages
+        const tehAis = createMenuItem('Teh Ais', 2.5, 'Tea sweetened with condensed milk that is simply served in a glass with ice cubes', 'Beverages', tehAisMenu);
+        menuItems.push(tehAis);
+
         //Main dishes
         const rotiCanai = createMenuItem('Roti Canai', 1.5, 'Soft and crispy flatbread served with curry and sambal', 'Main', rotiCanaiMenu);
         menuItems.push(rotiCanai)
@@ -49,3 +55,5 @@ const menu = (function() {
     
     return { initialize };
 })
+
+export default menu;
